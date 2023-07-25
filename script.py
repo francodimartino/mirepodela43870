7a1657c3f351c7d5193ed38d4f23dbd57e1ca4b1
FrancoDiMartino <francogdimartino@gmail.com>
2023-07-25 19:34:49 -0300

    modificado script.py y agregados datos en el txt

--- script.py
+++ script.py
@@ -1,4 +1,7 @@
 print("hola mundo!")
 nombre=input("ingrese nombre")
 #Esto es un comentario!
-print(f"Hola {nombre}!!!")
+print(f"Hola {nombre} como estas tantos años!!!")
+
+
+#estamos en la clase 16 aprendiendo git
